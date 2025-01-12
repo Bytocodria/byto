bc3c7ebe50ab3309441eb04a736d50e04162acf5
Milan Debnath <milandbnth@gmail.com>
2025-01-11 19:26:39 -0500

    creating website

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.style.display = 'none';
+    });
+    document.getElementById(pageId).style.display = 'block';
+}
